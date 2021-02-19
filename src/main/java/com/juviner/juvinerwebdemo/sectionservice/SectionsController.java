c2815277d81e28b60a3f9fe56e1731dec9482ea2
Valentino Giudice <valentino.giudice96@gmail.com>
2021-02-19 22:36:07 +0100

    Conf

--- src/main/java/com/juviner/juvinerwebdemo/sectionservice/SectionsController.java
+++ src/main/java/com/juviner/juvinerwebdemo/sectionservice/SectionsController.java
@@ -1,11 +1,13 @@
 package com.juviner.juvinerwebdemo.sectionservice;
 
-import java.util.List;
+import java.util.Optional;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "http://localhost:8030")
 @RestController
 @RequestMapping
 public class SectionsController {
@@ -28,5 +30,11 @@ public class SectionsController {
         Iterable<Section> sections = this.sectionDao.findAll();
         Root root = new Root("Lorem ipsum dolor sit amet, consectetur adipisci elit, sed do eiusmod tempor incidunt ut labore et dolore magna aliqua.", sections);
         return root;
+    }
+    
+    @GetMapping("/category/{id}")
+    public Category getCategory(@PathVariable int id) {
+        Optional<Category> category = this.categoryDao.findById(id);
+        return category.get();
     }
 }
